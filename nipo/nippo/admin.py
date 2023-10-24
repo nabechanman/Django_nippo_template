d58167aed3ec8b48f17a2d1ec759c913440a341c
watanabe ryota <nabeon0627@gmail.com>
2023-10-24 11:23:35 +0900

    first commit

---
+++ nipo/nippo/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from .models import NippoModel 
+
+admin.site.register(NippoModel) 
+
---
+++ nipo/nippo/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import nippoListView
+
+urlpatterns = [
+    path("", nippoListView),
+
+
+]
