675a93921fe82c04929c99ea790e39cc1daa1dbc
watanabe ryota <nabeon0627@gmail.com>
2023-10-25 12:28:52 +0900

    日程調整メール＿原文

--- nipo/nippo/views.py
+++ nipo/nippo/views.py
@@ -1,4 +1,29 @@
 from django.shortcuts import render
 
 def nippoListView(request):
-	return render(request, "nippo/nippo-list.html")
+	template_name="nippo/nippo-list.html"
+
+	if request.POST:
+		text = request.POST.get("text","")
+		name = request.POST.get("name","")
+		date = request.POST.get("date","")
+
+		input1_date = request.POST.get("input1_date","")
+		input1_time1 = request.POST.get("input1_time1","")
+		input1_time2 = request.POST.get("input1_time2","")
+
+
+	else :
+		text = ""
+		name = ""
+	ctx={}	
+	ctx["text"] = text
+	ctx["nn"] = name
+	ctx["date"]= date
+
+	ctx["input1_date"]  = input1_date
+	ctx["input1_time1"] = input1_time1
+	ctx["input1_time2"] = input1_time2
+
+	return render(request,template_name,ctx )
+
