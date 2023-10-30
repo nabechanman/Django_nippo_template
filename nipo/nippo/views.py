a2d4890a065637ef10f2066ed69323fbca4d49ed
watanabe ryota <nabeon0627@gmail.com>
2023-10-30 11:59:26 +0900

    異常なし

--- nipo/nippo/views.py
+++ nipo/nippo/views.py
@@ -8,26 +8,16 @@ def nippoListView(request):
     if request.POST:
         text = request.POST.get("text", "")
         name = request.POST.get("name", "")
-        content = request.POST.get("content","")
-
+        content = request.POST.get("content", "")
         input1_date = request.POST.get("input1_date", "")
-        s = str(input1_date).split("-")
-        p1 = s[1]+"月"+s[2]+"日"
-
         input1_time1 = request.POST.get("input1_time1", "")
         input1_time2 = request.POST.get("input1_time2", "")
 
         input2_date = request.POST.get("input2_date", "")
-        s = str(input2_date).split("-")
-        p2 = s[1]+"月"+s[2]+"日"
-
         input2_time1 = request.POST.get("input2_time1", "")
         input2_time2 = request.POST.get("input2_time2", "")
 
         input3_date = request.POST.get("input3_date", "")
-        s = str(input3_date).split("-")
-        p3 = s[1]+"月"+s[2]+"日"
-
         input3_time1 = request.POST.get("input3_time1", "")
         input3_time2 = request.POST.get("input3_time2", "")
 
@@ -35,22 +25,36 @@ def nippoListView(request):
         ctx["nn"] = name
         ctx["content"] = content
 
-        ctx["input1_date"] = p1
-        ctx["input1_time1"] = input1_time1
-        ctx["input1_time2"] = input1_time2
 
-        ctx["input2_date"] = p2
-        ctx["input2_time1"] = input2_time1
-        ctx["input2_time2"] = input2_time2
-
-        ctx["input3_date"] = p3
-        ctx["input3_time1"] = input3_time1
-        ctx["input3_time2"] = input3_time2
-
+    if input1_date:
+        s = str(input1_date).split("-")
+        p1 = s[1] + "月" + s[2] + "日"
     else:
-        text = ""
-        name = ""
+        p1 = ""
+
+    ctx["input1_date"] = p1
+    ctx["input1_time1"] = input1_time1
+    ctx["input1_time2"] = input1_time2
+
+   
+    if input2_date:
+        s = str(input2_date).split("-")
+        p2 = s[1] + "月" + s[2] + "日"
+    else:
+        p2 = ""
+
+    ctx["input2_date"] = p2
+    ctx["input2_time1"] = input2_time1
+    ctx["input2_time2"] = input2_time2
+
+    if input3_date:
+        s = str(input3_date).split("-")
+        p3 = s[1] + "月" + s[2] + "日"
+    else:
+        p3 = ""
+
+    ctx["input3_date"] = p3
+    ctx["input3_time1"] = input3_time1
+    ctx["input3_time2"] = input3_time2
 
     return render(request, template_name, ctx)
-
-
